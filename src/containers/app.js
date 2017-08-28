1d655b3e311f0ed7663d53f3614532dacc88946c
Mr-KOO <30999999+Mr-KOO@users.noreply.github.com>
2017-08-28 20:50:10 +0900

    feat: add header comp.

---
+++ src/containers/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from '../components/header';
+
+export class App extends React.Component{
+    render(){
+        return(
+            <div className="container">
+                <Header />
+            </div>
+        )
+    }
+}
